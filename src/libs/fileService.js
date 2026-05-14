7e1a5faab87658bf596c447931c934b6e7641bcb
Hasamuddin Afzali <hasamuddin.afz@gmail.com>
2026-05-14 12:11:49 +0300

    feat: file and user service

--- src/libs/fileService.js
+++ src/libs/fileService.js
@@ -1,4 +1,6 @@
 import api from "./api"
 
-export const uploadFile = (data) => api.post('/file/upload', data)
+export const uploadFile = (data) => api.post('/file/upload', data, {
+    headers: { 'Content-Type': 'multipart/form-data' }
+})
 export const deleteFile = (fileUrl) => api.delete(`/file/delete?fileUrl=${fileUrl}`)
--- src/libs/userService.js
+++ src/libs/userService.js
@@ -1,7 +1,9 @@
 import api from "./api"
 
-export const getAllUsers = () => api.get('/user')
-export const getUserById = (id) => api.get(`/user/${id}`)
-export const createUser = (data) => api.post('/user', data)
-export const updateUser = (id, data) => api.put(`/user/${id}`, data)
-export const deleteUser = (id) => api.delete(`/user/${id}`)
+export const getAllUsers = () => api.get('/users')
+export const getUserById = (id) => api.get(`/users/${id}`)
+export const createUser = (data) => api.post('/users', data)
+export const updateUser = (id, data) => api.put(`/users/${id}`, data)
+export const deleteUser = (id) => api.delete(`/users/${id}`)
+export const assignRole = (id, roleId) => api.post(`/users/${id}/roles`, { roleId })
+export const changePassword = (data) => api.post('/auth/change-password', data)
